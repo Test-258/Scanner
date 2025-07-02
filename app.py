3743daace749a7c3633ed65809a329c53baec080
Test-258 <test.the.test258@gmail.com>
2025-07-02 20:32:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def check():
     return jsonify({"phishing": phishing, "message": message})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
